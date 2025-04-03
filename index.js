24e15ca4b3ee569cf9098612d8d6c5fcb7b91c84
mhkmhsh <mahekmahesh12@gmail.com>
2025-04-02 21:34:40 -0400

    began the actual website

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-document.getElementById("menu-btn").addEventListener("click", function() {
-    let sidebar = document.getElementById("sidebar");
-    let button = document.getElementById("menu-btn");
-    sidebar.classList.toggle("active");
-    button.classList.toggle("active");
-});
 
-document.getElementById("backToTop").addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-});
 
